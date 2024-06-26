d7a4f944435ed527373849bb0e970b7eba503214
SarahLakey <salbers1212@gmail.com>
2024-06-25 23:11:37 -0500

    configured mysql database to sprint boot application

---
+++ src/main/java/org/launchcode/Walkabout_Backend/entity/User.java
@@ -0,0 +1,33 @@
+package org.launchcode.Walkabout_Backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "email")
+    private String email;
+
+}
