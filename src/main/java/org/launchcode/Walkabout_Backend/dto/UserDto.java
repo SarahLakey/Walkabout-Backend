7d919419da10203bf60856fd0da57ae54d21b6fe
SarahLakey <salbers1212@gmail.com>
2024-06-26 16:17:31 -0500

    added User DTO

---
+++ src/main/java/org/launchcode/Walkabout_Backend/dto/UserDto.java
@@ -0,0 +1,20 @@
+package org.launchcode.Walkabout_Backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String email;
+}
+
+//UserDto class transfers data between the client and the server
