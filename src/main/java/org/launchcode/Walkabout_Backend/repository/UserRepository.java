9afcb0285b603379b9f321e411593c69d1ade211
SarahLakey <salbers1212@gmail.com>
2024-06-26 16:13:27 -0500

    added UserRepository

---
+++ src/main/java/org/launchcode/Walkabout_Backend/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package org.launchcode.Walkabout_Backend.repository;
+
+import org.launchcode.Walkabout_Backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class UserRepository extends JpaRepository<User, Long> {
+
+
+
+}
