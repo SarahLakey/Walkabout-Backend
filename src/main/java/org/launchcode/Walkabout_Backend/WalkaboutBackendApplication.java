32bd90bd308c4bd4b3498884ece19c26a0cf0adf
SarahLakey <salbers1212@gmail.com>
2024-06-25 22:28:29 -0500

    added initial file tree using start.spring.io

---
+++ src/main/java/org/launchcode/Walkabout_Backend/WalkaboutBackendApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.Walkabout_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalkaboutBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalkaboutBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/Walkabout_Backend/WalkaboutBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.Walkabout_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalkaboutBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
