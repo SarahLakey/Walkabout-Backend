9514dbf963994ae10a6cafd6e8a5f5d6993fe13c
SarahLakey <salbers1212@gmail.com>
2024-07-07 23:11:50 -0500

    worked on delete user but getting 500 error message with postman

--- src/main/java/org/launchcode/Walkabout_Backend/controller/UserController.java
+++ src/main/java/org/launchcode/Walkabout_Backend/controller/UserController.java
@@ -3,7 +3,12 @@ package org.launchcode.Walkabout_Backend.controller;
 import lombok.RequiredArgsConstructor;
 import org.launchcode.Walkabout_Backend.entity.User;
 import org.launchcode.Walkabout_Backend.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -16,6 +21,23 @@ public  class UserController {
     @PostMapping("/user")
     public User postUser(@RequestBody User user){
         return userService.postUser(user);
+    }
+
+
+    @GetMapping("/users")
+    public List<User> getAllUsers(){
+        return userService.getAllUsers();
+    }
+    //used Postman to check Get All Users API http://localhost:8080/api/users
+
+    @DeleteMapping("/user/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable Long id){
+        try {
+            userService.deleteUser(id);
+            return new ResponseEntity<>("User with ID " + id + " successfully deleted", HttpStatus.OK);
+        }catch (EntityNotFoundException e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
     }
 
 }
--- src/main/java/org/launchcode/Walkabout_Backend/service/UserService.java
+++ src/main/java/org/launchcode/Walkabout_Backend/service/UserService.java
@@ -4,6 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.launchcode.Walkabout_Backend.entity.User;
 import org.launchcode.Walkabout_Backend.repository.UserRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -13,6 +17,19 @@ public class UserService {
 
     public User postUser(User user){
         return userRepository.save(user);
+    }
+
+    //return list of all users
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+    //delete user
+    public void deleteUser(Long id){
+        if (!userRepository.existsById(id)) {
+            throw new EntityNotFoundException("User with ID " + id + " not found");
+        }
+        userRepository.deleteById(id);
     }
 
 }
