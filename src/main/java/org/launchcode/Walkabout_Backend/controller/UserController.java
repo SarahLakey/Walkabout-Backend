7e557b387dcabb147a32e03e19d70c062d705e8b
SarahLakey <salbers1212@gmail.com>
2024-07-07 21:51:33 -0500

    added crossorigins to usercontroller to allow backend to communicate with frontend

--- src/main/java/org/launchcode/Walkabout_Backend/controller/UserController.java
+++ src/main/java/org/launchcode/Walkabout_Backend/controller/UserController.java
@@ -3,14 +3,12 @@ package org.launchcode.Walkabout_Backend.controller;
 import lombok.RequiredArgsConstructor;
 import org.launchcode.Walkabout_Backend.entity.User;
 import org.launchcode.Walkabout_Backend.service.UserService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
+@CrossOrigin("*") //allows backend to communicate with frontend
 public  class UserController {
 
     private final UserService userService;
